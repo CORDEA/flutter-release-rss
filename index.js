3354355784ebaa3e87ee1df3d835ddd2234cabb7
Yoshihiro Tanaka <contact@cordea.jp>
2023-09-23 14:31:39 +0900

    reformat code

--- index.js
+++ index.js
@@ -1,69 +1,65 @@
-const xml = require('xml')
-const {writeFile} = require('fs/promises')
+const xml = require("xml");
+const { writeFile } = require("fs/promises");
 
-const baseUrl = 'https://storage.googleapis.com/flutter_infra_release/releases'
+const baseUrl = "https://storage.googleapis.com/flutter_infra_release/releases";
 
-const query = [
-    'windows',
-    'macos',
-    'linux'
-]
+const query = ["windows", "macos", "linux"];
 
 async function fetchRelease(os) {
-    const response = await fetch(`${baseUrl}/releases_${os}.json`)
-    const data = await response.json()
-    const releases = data['releases']
-    const result = []
-    for (const release of releases) {
-        result.push({
-            item: [
-                {
-                    title: `Flutter v${release['version']} has been released`,
-                },
-                {
-                    link: `https://docs.flutter.dev/release/archive?tab=${os}`,
-                },
-                {
-                    description: `Hash: ${release['hash']}, Dart SDK Version: ${release['dart_sdk_version']}.`,
-                },
-                {
-                    pubDate: release['release_date']
-                }
-            ]
-        })
-    }
-    return result
+  const response = await fetch(`${baseUrl}/releases_${os}.json`);
+  const data = await response.json();
+  const releases = data["releases"];
+  const result = [];
+  for (const release of releases) {
+    result.push({
+      item: [
+        {
+          title: `Flutter v${release["version"]} has been released`,
+        },
+        {
+          link: `https://docs.flutter.dev/release/archive?tab=${os}`,
+        },
+        {
+          description: `Hash: ${release["hash"]}, Dart SDK Version: ${release["dart_sdk_version"]}.`,
+        },
+        {
+          pubDate: release["release_date"],
+        },
+      ],
+    });
+  }
+  return result;
 }
 
 function buildRss(os, items) {
-    const rss = [
+  const rss = [
+    {
+      rss: [
+        { _attr: { version: "2.0" } },
         {
-            rss: [
-                {_attr: {version: '2.0'}},
-                {
-                    channel: [
-                        {
-                            title: `Flutter releases for ${os}`
-                        },
-                        {
-                            link: 'https://docs.flutter.dev/release/release-notes'
-                        },
-                        {
-                            description: ''
-                        },
-                        ...items
-                    ]
-                }
-            ]
-        }
-    ]
-    return xml(rss, {declaration: true})
+          channel: [
+            {
+              title: `Flutter releases for ${os}`,
+            },
+            {
+              link: "https://docs.flutter.dev/release/release-notes",
+            },
+            {
+              description: "",
+            },
+            ...items,
+          ],
+        },
+      ],
+    },
+  ];
+  return xml(rss, { declaration: true });
 }
 
 (async function () {
-    for (const e of query) {
-        const items = await fetchRelease(e)
-        const result = buildRss(e, items)
-        await writeFile(`releases_${e}.xml`, result)
-    }
-})()
+  for (const e of query) {
+    const items = await fetchRelease(e);
+    const result = buildRss(e, items);
+    await writeFile(`releases_${e}.xml`, result);
+  }
+})();
