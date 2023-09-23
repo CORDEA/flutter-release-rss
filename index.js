73a27f2ecb3fecd7987869f5faf9b23b487a7313
Yoshihiro Tanaka <contact@cordea.jp>
2023-09-23 14:29:54 +0900

    build the XML

--- index.js
+++ index.js
@@ -1,12 +1,69 @@
-import fetch from 'node-fetch'
+const xml = require('xml')
+const {writeFile} = require('fs/promises')
 
-const urls = [
-    'https://storage.googleapis.com/flutter_infra_release/releases/releases_windows.json',
-    'https://storage.googleapis.com/flutter_infra_release/releases/releases_macos.json',
-    'https://storage.googleapis.com/flutter_infra_release/releases/releases_linux.json'
+const baseUrl = 'https://storage.googleapis.com/flutter_infra_release/releases'
+
+const query = [
+    'windows',
+    'macos',
+    'linux'
 ]
 
-for (const url in urls) {
-    const response = await fetch(url)
+async function fetchRelease(os) {
+    const response = await fetch(`${baseUrl}/releases_${os}.json`)
     const data = await response.json()
+    const releases = data['releases']
+    const result = []
+    for (const release of releases) {
+        result.push({
+            item: [
+                {
+                    title: `Flutter v${release['version']} has been released`,
+                },
+                {
+                    link: `https://docs.flutter.dev/release/archive?tab=${os}`,
+                },
+                {
+                    description: `Hash: ${release['hash']}, Dart SDK Version: ${release['dart_sdk_version']}.`,
+                },
+                {
+                    pubDate: release['release_date']
+                }
+            ]
+        })
+    }
+    return result
 }
+
+function buildRss(os, items) {
+    const rss = [
+        {
+            rss: [
+                {_attr: {version: '2.0'}},
+                {
+                    channel: [
+                        {
+                            title: `Flutter releases for ${os}`
+                        },
+                        {
+                            link: 'https://docs.flutter.dev/release/release-notes'
+                        },
+                        {
+                            description: ''
+                        },
+                        ...items
+                    ]
+                }
+            ]
+        }
+    ]
+    return xml(rss, {declaration: true})
+}
+
+(async function () {
+    for (const e of query) {
+        const items = await fetchRelease(e)
+        const result = buildRss(e, items)
+        await writeFile(`releases_${e}.xml`, result)
+    }
+})()
